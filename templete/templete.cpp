526b6b54e20cd90fa6b668c7713089736aa3d557
Shunichi Sakai <jmjm_jmjm@me.com>
2020-09-07 17:34:02 +0900

    make templete for contest

---
+++ templete/templete.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,N) for(int i=0; i<int(N); ++i)
+#define pb push_back
+#define ALL(obj) ((obj).begin(), (obj).end())
+
+int main() {
+    
+}
