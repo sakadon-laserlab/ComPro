67d3eae39ac001abd0ce7457a91285a30c4f4f3f
Shunichi Sakai <jmjm_jmjm@me.com>
2020-09-10 08:59:24 +0900

    add typedef to templete

--- templete/templete.cpp
+++ templete/templete.cpp
@@ -1,11 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
 #define FOR(i,a,b) for(int i=int(a); i<int(b); ++i)
 #define FORD(i,a,b) for(int i=int(a); i<int(b); --i)
 #define REP(i,N) for(int i=0; i<int(N); ++i)
 #define REPD(i,N) for(int i=0; i<int(N); --i)
-#define VV(type) vector<vector<(type)>>
 #define PB push_back
 #define ALL(obj) (obj).begin(), (obj).end()
 #define INF 1000000000000 //10^12:∞
@@ -13,5 +16,6 @@ typedef long long ll;
 #define MAXR 100000 //配列の最大のrenge
 
 int main() {
-    
+    VV(int) a(2,vector<int>(3,2));
+    cout <<
 }
