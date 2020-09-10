4555c809cad3b516b25930db5dc7d5ec56b93c8c
Shunichi Sakai <jmjm_jmjm@me.com>
2020-09-10 16:53:32 +0900

    add ARC031_b

--- practice/DFS/arc031_b.cpp
+++ practice/DFS/arc031_b.cpp
@@ -30,6 +30,47 @@ typedef vector<string> VS;
 #define dump(x)  cerr << #x << " = " << (x) << endl;
 #define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
 
+void dfs(VS G, int x, int y, VVB &seen) {
+    seen[x][y] = true;
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, -1, 0, 1};
+    REP(i,4) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if (nx<0 || nx>=10 || ny<0 || ny>=10) continue;
+        if (G[nx][ny] == 'x') continue;
+        if (seen[nx][ny] == true) continue;
+        dfs(G, nx, ny, seen);
+    }
+
+}
+
 int main() {
-    
+    VS G(10);
+    REP(i,10) cin>>G[i];
+
+    REP(i,10)REP(j,10) {
+        VVB seen(10,vector<bool>(10,false));
+        if (G[i][j] == 'o') continue;
+        G[i][j] = 'o';
+        bool onepeace = true;
+        bool res = true;
+        REP(k,10)REP(l,10) {
+            if (G[k][l] == 'o' && seen[k][l] == false) {
+                if (onepeace == false) {
+                    res = false;
+                    break;
+                }
+                onepeace = false;
+                dfs(G, k, l, seen); 
+            }
+        }
+        if (res == true) {
+
+            cout << "Yes" << endl;
+            return 0;
+        }
+        G[i][j] = 'x';
+    }
+    cout << "No" << endl;
 }
