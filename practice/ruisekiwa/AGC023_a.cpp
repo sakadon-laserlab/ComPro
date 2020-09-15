066a17d22b86875ec4b6e910e38f98cbe2b42020
Shunichi Sakai <jmjm_jmjm@me.com>
2020-09-15 15:43:07 +0900

    add ruisekiwa prac4

---
+++ practice/ruisekiwa/AGC023_a.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+//typedef
+typedef long long ll;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<bool> VB;
+typedef vector<VB> VVB;
+typedef vector<string> VS;
+//repetition
+#define FOR(i,a,b) for(int i=int(a); i<int(b); ++i)
+#define FORD(i,a,b) for(int i=int(a); i<int(b); --i)
+#define REP(i,N) for(int i=0; i<int(N); ++i)
+#define REPD(i,N) for(int i=0; i<int(N); --i)
+//container util
+#define ALL(obj) (obj).begin(), (obj).end()
+#define RALL(obj) (obj).rbegin(), (obj).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define SORT(c) sort((c).begin(),(c).end())
+//constant
+#define INF 1000000000000 //10^12:∞
+#define MOD 1000000007 //10^9+7
+#define MAXR 100000 //配列の最大のrenge
+//debug
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+int main() {
+    int N; cin >> N;
+    VI a(N); REP(i,N) cin >> a[i];
+
+    VI s(N+1,0);
+    map<int, int> nums;
+    REP(i,N) s[i+1] = s[i] + a[i];
+    REP(i,N+1) nums[s[i]]++;
+
+    ll res = 0;
+    for (auto it : nums) {
+        ll num = it.second;
+        res += num * (num -1) / 2; 
+    } 
+    cout << res << endl;
+}
