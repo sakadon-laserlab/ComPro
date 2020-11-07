e5b82e7f1853fa9eefb7fc68e66039eb7e651bba
Shunichi Sakai <s.sakai@laserlab.ed.kyushu-u.ac.jp>
2020-11-07 12:23:18 +0900

    add test

--- test.cpp
+++ test.cpp
@@ -35,24 +35,21 @@ template<class T> inline bool chmax(T &a, T b) {if (a < b){a = b; return true;} 
 #define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
 
 int main() {
-    int N; cin >> N;
-    VVL tate,yoko;
-    ll ans = 10000;
+    double a;
+    while (true) {
+        cout << "整数を入力してください : A=";
+        cin >> a;
 
-    REP(i,N) {
-        VL tmp(4); cin>>tmp[0]>>tmp[1]>>tmp[2]>>tmp[3];
-        if (tmp[0] == tmp[2]) tate.PB(tmp);
-        else yoko.PB(tmp);
+        if (cin.good() == 0) {
+            cout << "うんこ。やり直し。" << endl;
+            cin.clear();
+            cin.ignore(256,'\n');
+            continue;
+        }
+
+        else break;
     }
 
-    sort(ALL(tate));
-    sort(ALL(yoko),[](const vector<ll> &alpha,const vector<ll> &beta){return alpha[1] < beta[1];});
-
-    REP(i,tate.size()-1)FOR(j,i+1,tate.size())REP(k,yoko.size()-1)FOR(l,k+1,yoko.size()) {
-        if (tate[i][0]<yoko[k][0] || tate[i][0]<yoko[l][0] || tate[j][2]>yoko[k][2] || tate[j][2]>yoko[l][2]) continue;
-        if (yoko[l][3]>tate[i][3] || yoko[l][3]>tate[j][3] || yoko[k][1]<tate[i][1] || yoko[k][1]<tate[j][1]) continue;
-        ans = min(ans,(yoko[l][1]-yoko[k][1])*(tate[j][0]-tate[i][0]));
-    }
-
-    cout << ans << endl;
+    cout << "A = " << a << endl;
+    cout << "Aの階乗は" << pow(a,2) << endl;
 }
